36639d219bae32abecb3056171b193cad1c63683
Scott Umsted <scottumsted@gmail.com>
2015-10-24 08:07:43 -0500

    initial

---
+++ web.py
@@ -0,0 +1,30 @@
+import os
+from bottle import run, get, post, request, template, static_file, route
+
+
+@get('/')
+def get_index():
+    return template('index', images=[])
+
+
+@get('/images')
+def get_image():
+    return {'images': []}
+
+
+@post('/image')
+def post_message():
+    try:
+        message = request.json
+        if message is None:
+            message = {'user': 'echo', 'message': 'Huh, say something?'}
+    except Exception as e:
+        message = {'user': 'echo', 'message': 'Huh, what?'}
+    return {'status': 'SUCCESS'}
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='./static')
+
+
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
